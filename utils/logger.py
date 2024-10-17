8671d48b487da48e8d7f6fdf4ba55659f273e3de
nikitt <nikittaosipov@yandex.ru>
2024-10-17 21:46:49 +0400

    fix

--- utils/logger.py
+++ utils/logger.py
@@ -2,9 +2,8 @@ import logging
 
 
 def setup_logging():
-    """Настраивает логирование."""
+    """Настраивает логирование"""
     logging.basicConfig(filename='app.log',
                         filemode='a',
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    
