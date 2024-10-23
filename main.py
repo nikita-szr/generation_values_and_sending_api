34b7709ffca075ad8249ff3c1aebb580b7cdabf9
nikitt <nikittaosipov@yandex.ru>
2024-10-23 19:43:51 +0400

    обновлен алгоритм

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from utils.logger import setup_logging
 from utils.scheduler import schedule_task
 
-
-# конфигурация из файла
+# Конфигурация из файла
 def load_config(config_path='config.json'):
     with open(config_path) as f:
         return json.load(f)
-
 
 # Основная функция
 def main():
@@ -20,23 +18,22 @@ def main():
     config = load_config()
     api_url = config['api_url']
     token = config['token']
-    id = config['id']
-    number_of_values = config['number_of_values']  # Читаем количество значений
+    ids = config['ids']  # Три ID
+    number_of_values = config['number_of_values']  # Количество значений для генерации
 
     # Планировщик
     scheduler = BlockingScheduler()
 
     # Настройка расписания выполнения задачи
     scheduler.add_job(schedule_task, 'interval', weeks=1,
-                      args=[api_url, token, id, number_of_values],
+                      args=[api_url, token, ids, number_of_values],
                       next_run_time=config['next_run_time'])
 
-    logging.info("Скрипт запущен и ждет выполнения.")
+    logging.info("Скрипт запущен и ждёт выполнения.")
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         logging.info("Скрипт остановлен.")
-
 
 if __name__ == "__main__":
     main()
--- utils/scheduler.py
+++ utils/scheduler.py
@@ -3,14 +3,15 @@ from datetime import datetime
 from generator.generation import generate_values
 from sender.send_data import send_data
 
-
-def schedule_task(api_url, token, id, number_of_values):
-    """Функция для выполнения задачи: генерация значений и отправка через API."""
+def schedule_task(api_url, token, ids, number_of_values):
+    """Функция для выполнения задачи: генерация значений и отправка через API по разным ID."""
     logging.info(f"Начало выполнения задачи: {datetime.now()}")
 
-    # Генерация значений
+    # Генерация одного набора значений
     values = generate_values(number_of_values)
     logging.info(f"Сгенерированные значения: {values}")
 
-    # Отправка данных через API
-    send_data(api_url, token, id, values)
+    # Отправка одинаковых значений для всех указанных ID
+    for id_ in ids:
+        logging.info(f"Отправка данных для ID {id_}")
+        send_data(api_url, token, id_, values)
